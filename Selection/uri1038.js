54728f40bcbc0d96f642da2140aa14bd98cb72d5
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2021-09-08 20:58:52 -0300

    Uri 1038-v2

--- Selection/uri1038.js
+++ Selection/uri1038.js
@@ -1,24 +1,18 @@
-const input = require('fs').readFileSync('./stdin', 'utf8');
-const [cod, qtd] = input.split(' ').map(num => parseInt(num));
+const input = require('fs').readFileSync('./JavaScript-Uri/Selection/stdin', 'utf8');
+const [code, amount] = input.split(' ').map(num => parseInt(num));
 
-if (cod === 1) {
-    val = 4;
-    total = qtd * val;
-    console.log(`Total: R$ ${total.toFixed(2)}`)
-} else if (cod === 2) {
-    val = 4.5;
-    total = qtd * val;
-    console.log(`Total: R$ ${total.toFixed(2)}`)
-} else if (cod === 3) {
-    val = 5;
-    total = qtd * val;
-    console.log(`Total: R$ ${total.toFixed(2)}`)
-} else if (cod === 4) {
-    val = 2;
-    total = qtd * val;
-    console.log(`Total: R$ ${total.toFixed(2)}`)
-} else if (cod === 5) {
-    val = 1.5;
-    total = qtd * val;
-    console.log(`Total: R$ ${total.toFixed(2)}`)
+const snackValue = {
+    1: 4,
+    2: 4.5,
+    3: 5,
+    4: 2,
+    5: 1.5
+};
+
+for (let [key, value] of Object.entries(snackValue)) {
+    if (code == key) {
+        const total = amount * value;
+        console.log(`Total: R$ ${total.toFixed(2)}`);
+        break;
+    }
 }
