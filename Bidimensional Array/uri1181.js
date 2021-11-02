328b69d5bff3bf60fcc83fdab8cead4fcf5a19b0
Paulo Daniel Ambrosio <pda.ambrosio@gmail.com>
2021-11-02 10:34:39 -0300

    uri 1181

---
+++ Bidimensional Array/uri1181.js
@@ -0,0 +1,21 @@
+const input = require('fs').readFileSync('./JavaScript-Uri/Bidimensional Array/stdin', 'utf8');
+const values = input.split('\n');
+const [line, character] = values.slice(0, 2).map(value => values.shift());
+
+let sum = 0;
+let average = 0;
+let count = 0;
+
+for (let xArray = 0; xArray < 12; xArray++) {
+    for (let yArray = 0; yArray < 12; yArray++) {
+        const number = parseFloat(values.shift());
+
+        if (xArray === parseInt(line)) {
+            count += 1;
+            sum += number;
+        }
+    }
+}
+
+const result = character.toUpperCase() == 'S' ? sum.toFixed(1) : (sum / count).toFixed(1);
+console.log(result);
