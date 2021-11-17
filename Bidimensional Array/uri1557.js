9cedb5de22d2cfa5aa81f2fec1c8ad7888faf6e5
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2021-11-17 15:12:08 -0300

    uri 1557

---
+++ Bidimensional Array/uri1557.js
@@ -0,0 +1,42 @@
+const input = require('fs').readFileSync('./JavaScript-Uri/Bidimensional Array/stdin', 'utf8');
+const values = input.split('\n').map(value => parseInt(value));
+
+while (true) {
+    let number = values.shift();
+
+    if (number === 0) {
+        break;
+    }
+
+    if (number === 1) {
+        console.log(number);
+    } else {
+        const matrixArray = [];
+        let value = 1;
+    
+        for (let line = 0; line < number; line++) {
+            matrixArray[line] = new Array();
+            for (let column = 0; column < number; column++) {
+                matrixArray[line].push(value);
+                value *= 2;
+            }
+            value = matrixArray[line][1];
+        }
+    
+        const space = (matrixArray[number -1][number - 1]).toString();
+    
+        for (line = 0; line < number; line++) {
+            for (let column = 0; column < number; column++) {
+                matrixArray[line][column] = (matrixArray[line][column]).toString();
+
+                while (matrixArray[line][column].length < space.length) {
+                    matrixArray[line][column] = ' ' + matrixArray[line][column];
+                }
+
+            }
+            let result = matrixArray[line].join(' ');
+            console.log(result);
+        }
+    }
+    console.log();
+}
--- README.md
+++ README.md
@@ -40,3 +40,4 @@
 * [Uri1478 - Bidimensional Array](https://github.com/pdaambrosio/JavaScript-Uri/blob/main/Bidimensional%20Array/uri1478.js)
 * [Uri1534 - Bidimensional Array](https://github.com/pdaambrosio/JavaScript-Uri/blob/main/Bidimensional%20Array/uri1534.js)
 * [Uri1541 - Repetition](https://github.com/pdaambrosio/JavaScript-Uri/blob/main/Repetition/uri1541.js)
+* [Uri1557 - Bidimensional Array](https://github.com/pdaambrosio/JavaScript-Uri/blob/main/Bidimensional%20Array/uri1557.js)
