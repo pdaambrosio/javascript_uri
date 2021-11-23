d2e10f2d6f6d1512e9c8eddc35c4f8f717124465
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2021-11-23 19:47:09 -0300

    uri 1827

---
+++ Bidimensional Array/uri1827.js
@@ -0,0 +1,4 @@
+const input = require('fs').readFileSync('./JavaScript-Uri/Bidimensional Array/stdin', 'utf8');
+const values = input.split('\n').map(value => parseInt(value));
+
+// TODO: start the code
