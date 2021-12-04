19cfe7df3c786f4f5c19a04877af33da8386f4b5
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2021-12-03 21:20:21 -0300

    uri 1865

---
+++ Others/uri1865.js
@@ -0,0 +1,17 @@
+const input = require('fs').readFileSync('./JavaScript-Uri/Others/stdin', 'utf8');
+const values = input.split(/\s+/);
+const N = parseInt(values.shift());
+
+const mjolnir = (challenging) => {
+    for (let i = 0; i < challenging; i++) {
+        const [name, force] = values.splice(0, 2);
+
+        if (name == 'Thor') {
+            console.log('Y');
+        } else {
+            console.log('N');
+        }
+    }
+};
+
+mjolnir(N);
