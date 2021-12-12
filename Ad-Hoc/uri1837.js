f60a7b4c142df9e915e9591198195aa4a6c5e750
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2021-12-12 13:54:15 -0300

    ESLint - issue code: no-unused-vars

--- Ad-Hoc/uri1837.js
+++ Ad-Hoc/uri1837.js
@@ -2,9 +2,9 @@ const input = require('fs').readFileSync('./JavaScript-Uri/Ad-Hoc/stdin', 'utf8'
 const [A, B] = input.split(' ').map(number => parseInt(number));
 
 const quotientDivision = (num1, num2) => {
-    for (let i = 0;  i < Math.abs(B); i++) {
-        if ((A - i) % B === 0) {
-            let result = parseInt((A - i) / B);
+    for (let i = 0;  i < Math.abs(num2); i++) {
+        if ((num1 - i) % num2 === 0) {
+            let result = parseInt((num1 - i) / num2);
             console.log(result, i);
             break;
         }
