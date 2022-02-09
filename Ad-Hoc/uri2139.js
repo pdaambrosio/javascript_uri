d4f553f8aadb7e5a0ed030281a774197cfb3ef5a
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-02-09 08:25:15 -0300

    uri 2139 final

--- Ad-Hoc/uri2139.js
+++ Ad-Hoc/uri2139.js
@@ -1,8 +1,8 @@
 const input = require('fs').readFileSync('./JavaScript-Uri/Ad-Hoc/stdin', 'utf8');
-const values = input.split(/\s+/);
+const values = input.split(/\s+/).map(value => parseInt(value));
 
 const daysLeftUntilChristmas = (month, day) => {
-    const months = {1: 31, 2: 29, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 25};
+    const months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 25];
     let daysLeft = 0;
     
     if (month === 12) {
@@ -10,19 +10,29 @@ const daysLeftUntilChristmas = (month, day) => {
             return 'E vespera de natal!';
         } else if (day === 25) {
             return 'E natal!';
+        } else if (day > 24) {
+            return 'Ja passou!';
         } else if (day < 24) {
             return `Faltam ${25 - day} dias para o natal!`;
         }
     } else {
-        for (const value of Object.values(months)) {
-            while (month < 12) {
-                daysLeft += value;
+        for (let i = 0; i < months.length; i++) {
+            if (month <= 12) {
+                daysLeft += months[month - 1];
                 month += 1;
             }
-        } return `Faltam ${daysLeft} dias para o natal!`;
+        } return `Faltam ${daysLeft - day} dias para o natal!`;
     }
 };
 
 
+while (values !== 0) {
+    const [M, D] = values.splice(0, 2);
+    if (isNaN(M) || isNaN(D)) {
+        break;
+    } else {
+        console.log(daysLeftUntilChristmas(M, D));
+    }
+}
 
-console.log(daysLeftUntilChristmas(1, 5))
+// console.log(daysLeftUntilChristmas(11, 24));
