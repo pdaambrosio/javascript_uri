668b0b213b41cfe004337919ac793966442ba026
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-02-15 19:52:22 -0300

    uri 2143

---
+++ Unknow/uri2143.js
@@ -0,0 +1,23 @@
+const input = require('fs').readFileSync('./JavaScript-Uri/Unknow/stdin', 'utf8');
+const values = input.split('\n').map(value => parseInt(value));
+
+const theReturnOfRadar = (testCase) => {
+    if (testCase % 2 === 0) {
+        return (testCase * 2) - 2;
+    } else {
+        return (testCase * 2) - 1;
+    }
+};
+
+while (values !== 0) {
+    const T = values.shift();
+
+    if (T === 0) {
+        break;
+    }
+
+    for (let i = 0; i < T; i++) {
+        const test = values.shift();
+        console.log(theReturnOfRadar(test));
+    }
+}
