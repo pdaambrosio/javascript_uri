73880723a6fc38cfc4a8e3d195b286fc2b55eecf
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-03-18 14:58:13 -0300

    uri 2172

---
+++ Unknow/uri2172.js
@@ -0,0 +1,19 @@
+const input = require('fs').readFileSync('./javascript_uri/Unknow/stdin', 'utf8');
+const values = input.split(/\s+/).map(value => parseInt(value));
+
+const event = (xp) => {
+    const result = [];
+
+    while (xp.length !== 0) {
+        const [N, M] = xp.splice(0, 2);
+        if (N === 0 && M === 0) {
+            break;
+        }
+        result.push(N * M);
+    }
+    return result;    
+};
+
+for (let i of event(values)) {
+    console.log(i);
+}
