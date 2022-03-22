3e0f724a0c0fc8fa49781e53853fd8da26b9d597
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-03-22 13:06:32 -0300

    fix: indentation

--- Unknow/uri2172.js
+++ Unknow/uri2172.js
@@ -1,19 +1,22 @@
-const input = require('fs').readFileSync('./javascript_uri/Unknow/stdin', 'utf8');
-const values = input.split(/\s+/).map(value => parseInt(value));
+const input = require("fs").readFileSync(
+  "./javascript_uri/Unknow/stdin",
+  "utf8"
+);
+const values = input.split(/\s+/).map((value) => parseInt(value));
 
 const event = (xp) => {
-    const result = [];
+  const result = [];
 
-    while (xp.length !== 0) {
-        const [N, M] = xp.splice(0, 2);
-        if (N === 0 && M === 0) {
-            break;
-        }
-        result.push(N * M);
+  while (xp.length !== 0) {
+    const [N, M] = xp.splice(0, 2);
+    if (N === 0 && M === 0) {
+      break;
     }
-    return result;    
+    result.push(N * M);
+  }
+  return result;
 };
 
 for (let i of event(values)) {
-    console.log(i);
+  console.log(i);
 }
