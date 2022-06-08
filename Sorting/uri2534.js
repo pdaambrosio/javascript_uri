390ad7124135f4392a7d62d2de2be33390f3333d
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-06-08 08:06:44 -0300

    uri 2534

--- Sorting/uri2534.js
+++ Sorting/uri2534.js
@@ -4,4 +4,30 @@ const input = require("fs").readFileSync(
 );
 const values = input.split(/\s+/).map(Number);
 
-console.log(values);
+const sortingQueries = (queries) => {
+  const sorted = queries.sort(function (a, b) {
+    return b - a;
+  });
+  return sorted;
+};
+
+const gradeCitizen = (position, sorterdQueries) => {
+  return sorterdQueries[position - 1];
+};
+
+while (values.length > 0) {
+  const [n, q] = values.splice(0, 2);
+
+  if (isNaN(n) || isNaN(q)) {
+    break;
+  }
+
+  const inputQueries = values.splice(0, n);
+  const sortedQueries = sortingQueries(inputQueries);
+
+  for (let i = 0; i < q; i++) {
+    const position = values.shift();
+    const result = gradeCitizen(position, sortedQueries);
+    console.log(result);
+  }
+}
