142d50d260370b0337c92c6f6150bdd97cd23e26
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-06-22 07:54:35 -0300

    uri 2543 test function

--- Beginner/uri2543.js
+++ Beginner/uri2543.js
@@ -4,4 +4,23 @@ const input = require("fs").readFileSync(
 );
 const values = input.split(/\s+/).map((value) => parseInt(value));
 
-console.log(values);
+const ufprGaming = (loop, studentId) => {
+  let result = 0;
+  for (let i; i < loop; i++) {
+    const gameplay = values.splice(0, 2);
+    if (studentId === gameplay[0] && gameplay[1] === 0) {
+      result += 1;
+    }
+  }
+  return result;
+};
+
+while (values.length > 0) {
+  const [loop, studentId] = values.splice(0, 2);
+
+  if (isNaN(loop) || isNaN(studentId)) {
+    break;
+  }
+
+  console.log(ufprGaming(loop, studentId));
+}
