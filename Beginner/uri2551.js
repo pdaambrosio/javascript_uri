3bfb88f57542caa87c28337efcbad19f2cdb88a0
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-07-12 21:33:58 -0300

    uri 2551 get values

--- Beginner/uri2551.js
+++ Beginner/uri2551.js
@@ -2,3 +2,7 @@ const input = require("fs").readFileSync(
   "./javascript_uri/Beginner/stdin",
   "utf8"
 );
+
+const values = input.split(/\s+/).map((value) => parseInt(value));
+
+console.log(values);
