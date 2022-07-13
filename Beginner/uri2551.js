38cdc8ea3d985461f9da48a8ea00417331cee199
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-07-12 21:37:36 -0300

    uri 2551 variables

--- Beginner/uri2551.js
+++ Beginner/uri2551.js
@@ -2,7 +2,7 @@ const input = require("fs").readFileSync(
   "./javascript_uri/Beginner/stdin",
   "utf8"
 );
-
 const values = input.split(/\s+/).map((value) => parseInt(value));
+const loop = values.shift();
+const [trainingDuration, distance] = values.splice(0, 2);
 
-console.log(values);
