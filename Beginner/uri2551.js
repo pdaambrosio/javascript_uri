95e20516d13f086b43ca5e5a90ada6a94efbb6f8
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-07-20 20:42:35 -0300

    uri 2551

--- Beginner/uri2551.js
+++ Beginner/uri2551.js
@@ -2,7 +2,34 @@ const input = require("fs").readFileSync(
   "./javascript_uri/Beginner/stdin",
   "utf8"
 );
-const values = input.split(/\s+/).map((value) => parseInt(value));
-const loop = values.shift();
-const [trainingDuration, distance] = values.splice(0, 2);
+const values = input.split("\n");
 
+const newRecord = (loop) => {
+  const result = [];
+  let record = 0;
+  for (let i = 1; i < loop + 1; i++) {
+    const [trainingDuration, distance] = values
+      .shift()
+      .split(" ")
+      .map((value) => parseInt(value));
+
+    if (distance / trainingDuration > record) {
+      result.push(i);
+      record = distance / trainingDuration;
+    }
+  }
+  return result;
+};
+
+while (values.length > 0) {
+  const loop = parseInt(values.shift());
+  const records = newRecord(loop);
+
+  if (isNaN(loop)) {
+    break;
+  }
+
+  for (let i of records) {
+    console.log(i);
+  }
+}
