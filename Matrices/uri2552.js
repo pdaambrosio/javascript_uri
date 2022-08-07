5d07b64cbf65bd8973ef4f6f5893d2525d889037
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-08-07 11:28:54 -0300

    uri 2552

--- Matrices/uri2552.js
+++ Matrices/uri2552.js
@@ -3,14 +3,12 @@ const input = require("fs").readFileSync(
   "utf8"
 );
 const values = input.split(/\s+/).map((value) => parseInt(value));
-const [nLines, nColumns] = values.splice(0, 2);
 
 const cheeseBreadSweeper = (lines, columns) => {
   const matrix = [];
   for (let i = 0; i < lines; i++) {
     matrix[i] = values.splice(0, columns);
   }
-
   for (let i = 0; i < lines; i++) {
     for (let j = 0; j < columns; j++) {
       if (matrix[i][j] === 1) {
@@ -18,8 +16,39 @@ const cheeseBreadSweeper = (lines, columns) => {
       }
     }
   }
-
   return matrix;
 };
 
-console.log(cheeseBreadSweeper(nLines, nColumns));
+const cellWhithoutCheeseBread = (matrix, lines, columns) => {
+  let count = 0;
+  for (let i = 0; i < lines; i++) {
+    for (let j = 0; j < columns; j++) {
+      if (matrix[i][j] !== 9) {
+        if (i > 0 && matrix[i - 1][j] === 9) {
+          count++;
+        }
+        if (i < lines - 1 && matrix[i + 1][j] === 9) {
+          count++;
+        }
+        if (j > 0 && matrix[i][j - 1] === 9) {
+          count++;
+        }
+        if (j < columns - 1 && matrix[i][j + 1] === 9) {
+          count++;
+        }
+        matrix[i][j] = count;
+        count = 0;
+      }
+    }
+  }
+  return matrix;
+};
+
+while (values.length > 0) {
+  const [nLines, nColumns] = values.splice(0, 2);
+  const matrix = cheeseBreadSweeper(nLines, nColumns);
+  cellWhithoutCheeseBread(matrix, nLines, nColumns);
+  for (let i = 0; i < nLines; i++) {
+    console.log(matrix[i].join(""));
+  }
+}
