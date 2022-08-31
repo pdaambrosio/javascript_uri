92c7e162c46560ecd245d7fca54906fa8df3e592
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-08-31 08:40:57 -0300

    uri 2626 function init

---
+++ Unknow/uri2626.js
@@ -0,0 +1,31 @@
+const { verify } = require("crypto");
+
+const init = require("fs").readFileSync(
+  "./javascript_uri/Unknow/stdin",
+  "utf8"
+);
+
+const rockPaperScissors = (player1, player2, player3) => {
+  const dodo = "Os atributos dos monstros vao ser inteligencia, sabedoria...";
+  const leo = "Iron Maiden's gonna get you, no matter how far!";
+  const pepper = "Urano perdeu algo muito precioso...";
+  let result = "Putz vei, o Leo ta demorando muito pra jogar...";
+
+  switch (player1) {
+    case "papel":
+      if (player2 === "pedra" && player3 === "pedra") {
+        result = dodo;
+        break;
+      }
+    case "pedra":
+      if (player2 === "tesoura" && player3 === "tesoura") {
+        result = dodo;
+        break;
+      }
+    case "tesoura":
+      if (player2 === "papel" && player3 === "papel") {
+        result = dodo;
+      }
+  }
+  return result;
+};
