c02e89cfd4663c053340a9e2224772939f988880
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-10-10 08:32:54 -0300

    test: uri 2635 tests udebug

--- Unknow/uri2635.js
+++ Unknow/uri2635.js
@@ -9,4 +9,34 @@ const listSearch = values.splice(0, loopSearch);
 const loopQueries = parseInt(values.shift());
 const listQueries = values.splice(0, loopQueries);
 
-console.log(loopSearch, listSearch, loopQueries, listQueries);
+const webSearch = (loopSearch, loopQueries, listSearch, listQueries) => {
+  const result = [];
+  for (let querie of listQueries) {
+    let countSearch = 0;
+    let lenSearch = 0;
+    for (let search of listSearch) {
+      if (querie === search.substring(0, querie.length)) {
+        countSearch++;
+
+        if (lenSearch < search.length) {
+          lenSearch = search.length;
+        }
+      }
+    }
+    if (countSearch > 0) {
+      result.push(`${countSearch} ${lenSearch}`);
+    } else {
+      result.push("-1");
+    }
+  }
+  return result;
+};
+
+for (let result of webSearch(
+  loopSearch,
+  loopQueries,
+  listSearch,
+  listQueries
+)) {
+  console.log(result);
+}
