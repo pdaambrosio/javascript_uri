0d6687418ce927c30b3fb0f09f26d37e6e22c243
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-10-13 08:43:02 -0300

    uri 2653 Set constructor

--- Sets/uri2653.js
+++ Sets/uri2653.js
@@ -1,4 +1,5 @@
 const input = require("fs").readFileSync("./javascript_uri/Sets/stdin", "utf8");
 const values = input.split("\n");
+const result = new Set(values);
 
-console.log(values);
+console.log(result.size);
